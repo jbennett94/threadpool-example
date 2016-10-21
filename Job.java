0a274f1d97e768c2cef447c28a15ef9339f28aa4
jbennett94 <jolyonbennett@hotmail.co.uk>
2016-10-21 20:26:52 +0100

    Added the ThreadPoolApp file

--- Job.java
+++ Job.java
@@ -11,7 +11,7 @@ public class Job implements Runnable {
    System.out.println ("Job: " + jobNumber + " is being processed by thread : "
     + Thread.currentThread ().getName());
   try {
-        Thread.sleep((int)(1000);
+        Thread.sleep((int)(1000));
   } catch (InterruptedException e) {
         // no catching as example should not experience interruptions
   }
